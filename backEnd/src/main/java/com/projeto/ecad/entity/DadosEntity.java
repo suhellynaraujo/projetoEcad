faa55a60004d64d5077e24f4ff34c6c08aea23b4
suhellynaraujo <github.interface@gmail.com>
2022-04-21 19:58:34 -0300

    atributos

--- backEnd/src/main/java/com/projeto/ecad/entity/DadosEntity.java
+++ backEnd/src/main/java/com/projeto/ecad/entity/DadosEntity.java
@@ -1,5 +1,25 @@
 package com.projeto.ecad.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class DadosEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idDado;
+	private String nome;
+	private String dataInicial;
+	private String dataFinal;
+	private String arquivos;
+	
+	
+	
+	
+	
+	
 
 }
