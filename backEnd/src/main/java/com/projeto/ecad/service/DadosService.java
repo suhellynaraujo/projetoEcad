d77cef2e0912760e2e3d198922f2dd29ea9a58f5
suhellynaraujo <github.interface@gmail.com>
2022-04-21 20:32:06 -0300

    list, findAll

--- backEnd/src/main/java/com/projeto/ecad/service/DadosService.java
+++ backEnd/src/main/java/com/projeto/ecad/service/DadosService.java
@@ -1,5 +1,26 @@
 package com.projeto.ecad.service;
 
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.projeto.ecad.entity.DadosEntity;
+import com.projeto.ecad.repository.DadosRepository;
+
+@Service
 public class DadosService {
+	
+	@Autowired
+	private DadosRepository dadosRepository;
+	
+	@Transactional(readOnly = true)
+	public List<DadosEntity> findAll(){
+		List<DadosEntity> result = dadosRepository.findAll();
+		return result;
+		
+	}
 
 }
