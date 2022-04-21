aace0ed70ccec4a72914109ef7af2223dc5071cb
suhellynaraujo <github.interface@gmail.com>
2022-04-21 20:02:40 -0300

    métodos get e set

--- backEnd/src/main/java/com/projeto/ecad/entity/DadosEntity.java
+++ backEnd/src/main/java/com/projeto/ecad/entity/DadosEntity.java
@@ -27,6 +27,46 @@ public class DadosEntity {
 		this.dataFinal = dataFinal;
 		this.arquivos = arquivos;
 	}
+
+	public Long getIdDado() {
+		return idDado;
+	}
+
+	public void setIdDado(Long idDado) {
+		this.idDado = idDado;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDataInicial() {
+		return dataInicial;
+	}
+
+	public void setDataInicial(String dataInicial) {
+		this.dataInicial = dataInicial;
+	}
+
+	public String getDataFinal() {
+		return dataFinal;
+	}
+
+	public void setDataFinal(String dataFinal) {
+		this.dataFinal = dataFinal;
+	}
+
+	public String getArquivos() {
+		return arquivos;
+	}
+
+	public void setArquivos(String arquivos) {
+		this.arquivos = arquivos;
+	}
 	
 	
 	
