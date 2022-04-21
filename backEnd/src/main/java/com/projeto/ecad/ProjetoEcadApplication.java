684355292c18d3d328830d18349174d329820247
suhellynaraujo <github.interface@gmail.com>
2022-04-21 19:18:51 -0300

    criando o repositório do back

---
+++ backEnd/src/main/java/com/projeto/ecad/ProjetoEcadApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.ecad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoEcadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoEcadApplication.class, args);
+	}
+
+}
---
+++ backEnd/src/test/java/com/projeto/ecad/ProjetoEcadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.ecad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoEcadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
