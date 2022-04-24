650c39f38b13a9a7092063c1e454ceeba49780cc
suhellynaraujo <github.interface@gmail.com>
2022-04-23 21:47:02 -0300

    formulário simples

--- frontend/src/Pages/Index.jsx
+++ frontend/src/Pages/Index.jsx
@@ -1,6 +1,38 @@
+export default function Index() {
+  return (
+    <>
+      <main ClassName="principal">
+        <div ClassName="divSperior">
+          <div ClassName="filtroDePesquisa">
+            <h5>Filtro de Pesquisa</h5>
+          </div>
 
-export default function Index(){
-    return(
-        
-    )
+          <div ClassName="inserirDados">
+            <form>
+              <label> Nome
+              <input type="text" id="name" name="nome" />
+              </label>
+              <label> Data Inicial:
+              <input type="text" id="dataInicial" name="dataInicial" />
+              </label>
+              <label>Data Final:
+                  <input type="text" id="dataFinal" name="dataFinal" />
+              </label>
+              <label> Arquivo:
+                  <section>
+                      <option value="todos">Todos</option>
+                  </section>
+              </label>
+            </form>
+          </div>
+
+          <div ClassName="botoes">
+              <button>Pesquisar</button>
+              <button>Limpar</button>
+          </div>
+        </div>
+        <div className="divInferior"></div>
+      </main>
+    </>
+  );
 }
