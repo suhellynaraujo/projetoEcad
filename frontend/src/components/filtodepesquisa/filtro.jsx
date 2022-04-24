671943c5158b93bad96a79ba7feeb6d1d2ce54a5
suhellynaraujo <github.interface@gmail.com>
2022-04-24 16:04:43 -0300

    estilização

--- frontend/src/components/filtodepesquisa/filtro.jsx
+++ frontend/src/components/filtodepesquisa/filtro.jsx
@@ -1,36 +1,62 @@
-import "../filtodepesquisa/filtro.css"
-export default function Filtro(){
-    return(
-        <>
-        <div ClassName="divSuperior">
-          <div ClassName="filtroDePesquisa">
-            <h5>Filtro de Pesquisa</h5>
-          </div>
+import '../filtodepesquisa/filtro.css';
+import Lupa from "../../assets/img/lupa.svg";
+import Lixo from "../../assets/img/lixo.svg";
+import Calendario from "../../assets/img/calendar3.svg"
 
-          <div ClassName="inserirDados">
-            <form ClassName="formularioDados">
-              <label> Nome
-              <input type="text" id="name" name="nome" />
-              </label>
-              <label> Data Inicial:
-              <input type="text" id="dataInicial" name="dataInicial" />
-              </label>
-              <label>Data Final:
-                  <input type="text" id="dataFinal" name="dataFinal" />
-              </label>
-              <label> Arquivo:
-                  <section>
-                      <option value="todos">Todos</option>
-                  </section>
-              </label>
-            </form>
-          </div>
-
-          <div ClassName="botoes">
-              <button>Pesquisar</button>
-              <button>Limpar</button>
-          </div>
+export default function Filtro() {
+  return (
+    <>
+      <div className="divSuperior">
+        <div className="filtroDePesquisa">
+          <img src={Lupa} className="lupa" alt="pesquisar"/>
+          <h5>Filtro de Pesquisa</h5>
         </div>
-        </>
-    )
+
+        <div className="inserirDados">
+          <form className="formularioDados">
+            <div className="center">
+            <label >             
+              Nome
+              <input type="text" id="name" name="nome" />
+            </label>
+            </div>
+            <div className="divData">
+              <label>              
+                Data Inicial:
+                <input type="text" id="dataInicial" name="dataInicial" />
+                <img src={Calendario} className="calendario" alt="calendario"/>
+              </label>
+              <label>
+                Data Final:
+                <input type="text" id="dataFinal" name="dataFinal" />
+                <img src={Calendario} className="calendario" alt="calendario"/>
+              </label>
+            </div>
+            <div className="center">            
+              <label>           
+              Arquivo:
+              <select>
+                <option value="todos">TODOS</option>
+                <option value="tipo">TIPO</option>
+                <option value="arquivo">ARQUIVO</option>
+                <option value="dataInicial">DATA INICIAL</option>
+                <option value="dataFinal">DATA FINAL</option>
+                <option value="total">TOTAL</option>
+              </select>
+            </label>
+            </div>
+          </form>
+        </div>
+
+        <div className="botoes">
+          <button className="btn">
+          <img src={Lupa} className="lupa" alt="pesquisar"/>
+            Pesquisar</button>
+          <button className="btn">
+            <img src={Lixo} className="lixo" alt="excluir"/>
+            Limpar</button>
+        </div>
+      </div>
+    </>
+  );
 }
