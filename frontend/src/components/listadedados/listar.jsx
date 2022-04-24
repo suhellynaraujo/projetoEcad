1484ffbd209bbea8cf103cf653ae701adcfd3fc8
suhellynaraujo <github.interface@gmail.com>
2022-04-24 16:12:34 -0300

    criando método get na api

--- frontend/src/components/listadedados/listar.jsx
+++ frontend/src/components/listadedados/listar.jsx
@@ -1,8 +1,24 @@
+import { useEffect, useState } from "react"
+import api from "../../server/api"
 
 export default function Listar() {
+
+const [dados, setDados] = useState([])
+
+useEffect(() => {
+    api.get('/dados')
+    .then((response) =>{
+        setDados(response.data)
+    })
+    .catch((error) => console.log(error))
+}, [])
+
     return(
         <>
         <div className="divInferior">
+            {dados.map((dados) =>(
+                
+            ))}
             
         </div>
         </>
